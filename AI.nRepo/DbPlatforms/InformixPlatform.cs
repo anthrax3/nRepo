37e82f58e93a4274987e69f9380e9908c757febd
Sam Martindale <sam.martindale@theAIteam.com>
2013-07-17 21:24:53 -0500

    informix support.

---
+++ AI.nRepo/DbPlatforms/InformixPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FluentNHibernate.Cfg.Db;
+namespace AI.nRepo.DbPlatforms
+{
+    public class InformixPlatform : IDatabasePlatform
+    {
+
+        public object AsNHibernateConfiguration(string connectionString)
+        {
+            return FluentNHibernate.Cfg.Db.IfxOdbcConfiguration.Informix.ConnectionString(connectionString);
+        }
+    }
+}
